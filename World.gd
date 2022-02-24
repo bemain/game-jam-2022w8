2f7c2da5c79b3eda18e0f471640881f4196b87f4
BeMain <bemain.dev@gmail.com>
2022-02-24 22:01:24 +0100

    Cleanup mesh creation code

--- World.gd
+++ World.gd
@@ -1,27 +1,34 @@
 extends Spatial
 
 
-func _ready():
-	create_room()
+onready var room = create_room()
 
 
 func create_room():
 	var room2d = $Room
+	var room3d = Spatial.new()
+	room3d.name = "Room"
 	# Generate walls
 	for i in range(4):
 		var mesh_instance = create_mesh(room2d.get_node("Wall" + str(i + 1)))
+		mesh_instance.name = "Wall" + str(i + 1)
 		# Position the mesh
 		mesh_instance.rotate(Vector3(1, 0, 0), -PI / 2)
 		mesh_instance.rotate(Vector3(0, 1, 0), i * PI / 2)
 		mesh_instance.translation = Vector3(0, 0, 0.5).rotated(Vector3(0, 1, 0), i * PI / 2)
-		add_child(mesh_instance)
+		room3d.add_child(mesh_instance)
 	# Generate ceilings
 	var mesh_instance = create_mesh(room2d.get_node("Ceiling"))
+	mesh_instance.name = "Ceiling"
+	# Position the mesh
 	mesh_instance.rotate(Vector3(1, 0, 0), PI)
 	mesh_instance.translation = Vector3(0, 0.5, 0)
-	add_child(mesh_instance)
+	room3d.add_child(mesh_instance)
 	
 	remove_child(room2d)
+	
+	add_child(room3d)
+	return room3d
 
 
 func create_mesh(surface):
@@ -30,8 +37,10 @@ func create_mesh(surface):
 	surface.visible = true
 	# Create viewport
 	var viewport = Viewport.new()
+	viewport.name = "Viewport"
 	viewport.size = Constants.wallsize
 	viewport.set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+	viewport.physics_object_picking = true
 	viewport.add_child(surface)
 	# Create mesh
 	var mesh_instance = MeshInstance.new()
