7eab6f22fcd5966b19e6c96eabf141dc5cea2e72
BeMain <bemain.dev@gmail.com>
2022-02-24 12:53:34 +0100

    Generate viewports from walls and ceiling

--- World.gd
+++ World.gd
@@ -1,7 +1,24 @@
-extends Node2D
+extends Spatial
+ 
+var viewports = []
+
 
 func _ready():
-	var scale_val = min(Constants.screensize.x / Constants.viewsize.x, Constants.screensize.y / Constants.viewsize.y)
-	$ViewportContainer.rect_scale = Vector2(scale_val, scale_val)
-	$ViewportContainer.rect_position = Constants.screensize / 2 - scale_val * Constants.viewsize / 2
+	create_viewports()
 
+func create_viewports():
+	var room = $Room
+	var surfaces = []
+	# Get walls and ceiling
+	for i in range(4):
+		surfaces.append(room.get_node("Wall" + str(i + 1)))
+	surfaces.append(room.get_node("Ceiling"))
+	
+	# Generate viewports
+	for surface in surfaces:
+		var viewport = Viewport.new()
+		surface.visible = true
+		viewport.size = Constants.wallsize
+		viewport.add_child(surface)
+		viewports.append(viewport)
+	remove_child(room)
