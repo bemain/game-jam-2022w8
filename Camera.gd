376be0bfd4318b89fb804bd1523bfeb804dd8765
BeMain <bemain.dev@gmail.com>
2022-02-24 12:37:35 +0100

    Create 3D camera

--- Camera.gd
+++ Camera.gd
@@ -1,67 +1,98 @@
-extends Camera2D
+extends Camera
 
-enum {WALL, CEILING}
+export(float, 0.0, 1.0) var sensitivity = 0.25
 
+# Mouse state
+var _mouse_position = Vector2(0.0, 0.0)
+var _total_pitch = 0.0
 
-onready var ceiling = get_node("../Room").get_node("Ceiling")
-var walls = []
+# Movement state
+var _direction = Vector3(0.0, 0.0, 0.0)
+var _velocity = Vector3(0.0, 0.0, 0.0)
+var _acceleration = 30
+var _deceleration = -10
+var _vel_multiplier = 4
 
-var selected_wall = 0 setget set_selected_wall
-var selected_type = WALL
-
-func set_selected_wall(value):
-	selected_wall = fposmod(value, 4)
-
-
-func _ready():
-	# Get walls
-	for i in range(4):
-		walls.append(get_node("../Room").get_node("Wall" + str(i + 1)))
+# Keyboard state
+var _w = false
+var _s = false
+var _a = false
+var _d = false
+var _q = false
+var _e = false
 
 func _input(event):
-	# Block movement if already moving
-	if (position - get_camera_screen_center()).length() > 10:
-		return
-	match selected_type:
-		WALL:
-			if event.is_action_pressed("ui_right"):
-				move(Vector2.RIGHT)
-			if event.is_action_pressed("ui_left"):
-				move(Vector2.LEFT)
-			if event.is_action_pressed("ui_up"):
-				selected_type = CEILING
-				move(Vector2.UP)
-		CEILING:
-			if event.is_action_pressed("ui_down"):
-				selected_type = WALL
-				move(Vector2.DOWN)
+	# Receives mouse motion
+	if event is InputEventMouseMotion:
+		_mouse_position = event.relative
+	
+	# Receives mouse button input
+	if event is InputEventMouseButton:
+		match event.button_index:
+			BUTTON_RIGHT: # Only allows rotation if right click down
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
+			BUTTON_WHEEL_UP: # Increases max velocity
+				_vel_multiplier = clamp(_vel_multiplier * 1.1, 0.2, 20)
+			BUTTON_WHEEL_DOWN: # Decereases max velocity
+				_vel_multiplier = clamp(_vel_multiplier / 1.1, 0.2, 20)
 
+	# Receives key input
+	if event is InputEventKey:
+		match event.scancode:
+			KEY_W:
+				_w = event.pressed
+			KEY_S:
+				_s = event.pressed
+			KEY_A:
+				_a = event.pressed
+			KEY_D:
+				_d = event.pressed
+			KEY_Q:
+				_q = event.pressed
+			KEY_E:
+				_e = event.pressed
+
+# Updates mouselook and movement every frame
 func _process(delta):
-	# If moving, apply perspective to the walls
-	if (position - get_camera_screen_center()).length() > 10:
-		apply_perspective()
+	_update_mouselook()
+	_update_movement(delta)
 
-
-func move(direction: Vector2):
-	# Move
-	var target_pos = position + Constants.wallsize * direction
-	position = target_pos
-	# Set position of wall / ceiling
-	if direction.x:
-		self.selected_wall += direction.x
-		walls[selected_wall].position.x = target_pos.x
-	if direction.y:
-		ceiling.position.x = target_pos.x
-		ceiling.rotation = selected_wall * PI / 2
+# Updates camera movement
+func _update_movement(delta):
+	# Computes desired direction from key states
+	_direction = Vector3(_d as float - _a as float, 
+						 _e as float - _q as float,
+						 _s as float - _w as float)
 	
-
-func apply_perspective():
-	# Walls
-	for wall in walls:
-		var rel_pos = (get_camera_screen_center() - wall.position) / Constants.wallsize
-		if -2 < rel_pos.x and rel_pos.x < 2:
-			wall.material.set_shader_param("position", rel_pos)
+	# Computes the change in velocity due to desired direction and "drag"
+	# The "drag" is a constant acceleration on the camera to bring it's velocity to 0
+	var offset = _direction.normalized() * _acceleration * _vel_multiplier * delta \
+		+ _velocity.normalized() * _deceleration * _vel_multiplier * delta
 	
-	# Ceiling
-	var rel_pos = (get_camera_screen_center() - ceiling.position) / Constants.wallsize
-	ceiling.material.set_shader_param("position", rel_pos.rotated(-ceiling.rotation))
+	# Checks if we should bother translating the camera
+	if _direction == Vector3.ZERO and offset.length_squared() > _velocity.length_squared():
+		# Sets the velocity to 0 to prevent jittering due to imperfect deceleration
+		_velocity = Vector3.ZERO
+	else:
+		# Clamps speed to stay within maximum value (_vel_multiplier)
+		_velocity.x = clamp(_velocity.x + offset.x, -_vel_multiplier, _vel_multiplier)
+		_velocity.y = clamp(_velocity.y + offset.y, -_vel_multiplier, _vel_multiplier)
+		_velocity.z = clamp(_velocity.z + offset.z, -_vel_multiplier, _vel_multiplier)
+	
+		translate(_velocity * delta)
+
+# Updates mouse look 
+func _update_mouselook():
+	# Only rotates mouse if the mouse is captured
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		_mouse_position *= sensitivity
+		var yaw = _mouse_position.x
+		var pitch = _mouse_position.y
+		_mouse_position = Vector2(0, 0)
+		
+		# Prevents looking up/down too far
+		pitch = clamp(pitch, -90 - _total_pitch, 90 - _total_pitch)
+		_total_pitch += pitch
+	
+		rotate_y(deg2rad(-yaw))
+		rotate_object_local(Vector3(1,0,0), deg2rad(-pitch))
--- Constants.gd
+++ Constants.gd
@@ -3,4 +3,3 @@ extends Node
 
 const wallsize = Vector2(1024, 1024)
 onready var screensize = get_viewport().size
-onready var viewsize = get_tree().root.get_node("World/ViewportContainer/Viewport").size
