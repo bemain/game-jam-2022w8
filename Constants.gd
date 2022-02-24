9e629d92f03daf135b6f23c4457ed42b708e9390
BeMain <bemain.dev@gmail.com>
2022-02-24 12:27:22 +0100

    Add screensize and viewsize constants

--- Constants.gd
+++ Constants.gd
@@ -2,3 +2,5 @@ extends Node
 
 
 const wallsize = Vector2(1024, 1024)
+onready var screensize = get_viewport().size
+onready var viewsize = get_tree().root.get_node("World/ViewportContainer/Viewport").size
--- World.gd
+++ World.gd
@@ -1,10 +1,7 @@
 extends Node2D
 
-onready var screensize = get_viewport().size
-onready var viewsize = $ViewportContainer/Viewport.size
-
 func _ready():
-	var scale_val = min(screensize.x / viewsize.x, screensize.y / viewsize.y)
+	var scale_val = min(Constants.screensize.x / Constants.viewsize.x, Constants.screensize.y / Constants.viewsize.y)
 	$ViewportContainer.rect_scale = Vector2(scale_val, scale_val)
-	$ViewportContainer.rect_position = screensize / 2 - scale_val * viewsize / 2
+	$ViewportContainer.rect_position = Constants.screensize / 2 - scale_val * Constants.viewsize / 2
 
