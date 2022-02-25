47d58cfaad9032c4e84ce65f5968df1416fdb828
BeMain <bemain.dev@gmail.com>
2022-02-25 11:47:01 +0100

    Create 3D room from 2D PackedScene

--- World.gd
+++ World.gd
@@ -1,36 +1,36 @@
 extends Spatial
 
 
-onready var room = create_room()
+export(PackedScene) var room_template
+
+onready var room = create_room(room_template)
 var Surface3d = preload("res://Surface.tscn")
 
 
 
-func create_room():
-	var room2d = $Room
-	var room3d = Spatial.new()
-	room3d.name = "Room"
+func create_room(template_packed):
+	var template = template_packed.instance()
+	var room = Spatial.new()
+	room.name = "Room"
 	# Generate walls
 	for i in range(4):
-		var static_body = create_3d_object(room2d.get_node("Wall" + str(i + 1)))
+		var static_body = create_3d_object(template.get_node("Wall" + str(i + 1)))
 		static_body.name = "Wall" + str(i + 1)
 		# Position the mesh
 		static_body.rotate(Vector3(1, 0, 0), -PI / 2)
 		static_body.rotate(Vector3(0, 1, 0), i * PI / 2)
 		static_body.translation = Vector3(0, 0, 0.5).rotated(Vector3(0, 1, 0), i * PI / 2)
-		room3d.add_child(static_body)
+		room.add_child(static_body)
 	# Generate ceilings
-	var static_body = create_3d_object(room2d.get_node("Ceiling"))
+	var static_body = create_3d_object(template.get_node("Ceiling"))
 	static_body.name = "Ceiling"
 	# Position the mesh
 	static_body.rotate(Vector3(1, 0, 0), PI)
 	static_body.translation = Vector3(0, 0.5, 0)
-	room3d.add_child(static_body)
+	room.add_child(static_body)
 	
-	remove_child(room2d)
-	
-	add_child(room3d)
-	return room3d
+	add_child(room)
+	return room
 
 
 func create_3d_object(surface):
