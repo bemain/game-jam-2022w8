c46356b1d70ecdea24775d7c2cb99f3087ddfe27
BeMain <bemain.dev@gmail.com>
2022-02-26 11:12:18 +0100

    Allow for not cubic rooms

--- World.gd
+++ World.gd
@@ -19,34 +19,35 @@ func create_room(template_packed):
 		static_body.name = "Wall" + str(i + 1)
 		# Position the mesh
 		static_body.rotation = Vector3(-PI / 2, i * -PI / 2, 0)
-		static_body.translation = Vector3(0, 0, 0.5).rotated(Vector3(0, 1, 0), i * -PI / 2)
+		static_body.translation = Vector3(0, 0, (Constants.wallsize.x / Constants.wallsize.y) / 2).rotated(Vector3(0, 1, 0), i * -PI / 2)
 		room.add_child(static_body)
 	# Generate ceiling
-	var static_body = create_3d_object(template.get_node("Ceiling"))
+	var static_body = create_3d_object(template.get_node("Ceiling"), false)
 	static_body.name = "Ceiling"
 	static_body.rotation = Vector3(PI, 0, 0)
-	static_body.translation = Vector3(0, 0.5, 0)
+	static_body.translation = Vector3(0, (Constants.wallsize.y / Constants.wallsize.x) / 2, 0)
 	room.add_child(static_body)
 	# Generate floor
-	static_body = create_3d_object(template.get_node("Floor"))
+	static_body = create_3d_object(template.get_node("Floor"), false)
 	static_body.name = "Floor"
-	static_body.translation = Vector3(0, -0.5, 0)
+	static_body.translation = Vector3(0, -(Constants.wallsize.y / Constants.wallsize.x) / 2, 0)
 	room.add_child(static_body)
 	
 	add_child(room)
 	return room
 
 
-func create_3d_object(surface):
+func create_3d_object(surface, is_wall=true):
+	var quad_wall_size = Vector2(Constants.wallsize.x, Constants.wallsize.x)
 	# Configure surface
 	surface.get_parent().remove_child(surface)
-	surface.position = Constants.wallsize / 2 if surface.centered else Vector2.ZERO
+	surface.position = Vector2.ZERO if not surface.centered else (Constants.wallsize / 2 if is_wall else quad_wall_size / 2)
 	surface.visible = true
 	# Create Surface3d
 	var surface3d = Surface3d.instance()
 	# Configure viewport
 	var viewport = surface3d.get_node("Viewport")
-	viewport.size = Constants.wallsize
+	viewport.size = Constants.wallsize if is_wall else quad_wall_size
 	viewport.set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
 	viewport.physics_object_picking = true
 	viewport.add_child(surface)
