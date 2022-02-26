edd2b88467bda22cb1cf57e3e3747961091c2c65
BeMain <bemain.dev@gmail.com>
2022-02-26 16:57:54 +0100

    Register items and objects on startup

--- Prefabs/Objects/LockedObject.gd
+++ Prefabs/Objects/LockedObject.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
-export(String) var key = "Key"
+export(String) var object_name = "LockedObject_TEST"
+export(String) var key = "Key_TEST"
+
+
+func _ready():
+	# Register item
+	assert(not object_name in Gamestate.objects, "ERROR: Duplicate, item already exists: " + object_name)
+	Gamestate.objects[object_name] = self
 
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
