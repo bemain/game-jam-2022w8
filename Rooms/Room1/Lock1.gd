008d0537ff5965a1cc095fcfab3e9bb00dcc838c
BeMain <bemain.dev@gmail.com>
2022-02-27 18:44:46 +0100

    Make Lock1 unlockable

--- Rooms/Room1/Lock1.gd
+++ Rooms/Room1/Lock1.gd
@@ -1,2 +1,11 @@
 extends GameObject
 
+
+onready var animated_sprite = $AnimatedSprite
+
+
+func _ready():
+	animated_sprite.animation = "Locked"
+
+func _on_Unlockable_unlocked():
+	animated_sprite.animation = "Unlocked"
