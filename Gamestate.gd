a156cef89e8a5e25b5d9210635743de497d6ddfa
BeMain <bemain.dev@gmail.com>
2022-02-28 20:41:37 +0100

    Add signal focused_object_changed

--- Gamestate.gd
+++ Gamestate.gd
@@ -3,6 +3,7 @@ extends Node
 
 signal current_day_changed
 signal inventory_changed
+signal focused_object_changed
 
 
 export(int) var current_day = 0 setget set_current_day
@@ -12,7 +13,7 @@ export(Array, String) var inventory_items
 export(String) var selected_item
 
 export(Dictionary) var objects
-export(String) var focused_object
+export(String) var focused_object setget set_focused_object
 
 
 func set_current_day(value):
@@ -22,3 +23,7 @@ func set_current_day(value):
 func add_to_inventory(value):
 	inventory_items.append(value)
 	emit_signal("inventory_changed")
+
+func set_focused_object(value):
+	focused_object = value
+	emit_signal("focused_object_changed")
