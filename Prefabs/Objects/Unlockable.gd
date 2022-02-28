7f76eb82b4fb6eebb294e14cb540ae4bf7f59975
BeMain <bemain.dev@gmail.com>
2022-02-28 20:41:22 +0100

    Make Unlockable not inherit Focusable
    Objects using Unlockable are now responsible for disabling it
    when object is not focused

--- Prefabs/Objects/Unlockable.gd
+++ Prefabs/Objects/Unlockable.gd
@@ -1,4 +1,4 @@
-extends Focusable
+extends GameObject
 class_name Unlockable
 
 signal unlocked
@@ -8,15 +8,12 @@ export(String) var key = ""
 var is_locked = true
 
 
-func _on_Lock_input_event(viewport, event, shape_idx):
+func _on_Unlockable_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
 	and event.pressed:
 		if not is_locked:
 			return  # If already unlocked, return
-		
-		if Gamestate.focused_object != object_name:
-			return  # If object isn't focused, return
 		
 		if Gamestate.selected_item == key:
 			yield(get_tree(),"idle_frame")  # Wait one frame before focusing to let input pass to children first
