64ddf4140ed6761ffc0580102b83a6fb2c7d3d58
BeMain <bemain.dev@gmail.com>
2022-03-19 16:37:40 +0100

    Allow easily disabling Clickable via is_clickable

--- Prefabs/Objects/Clickable.gd
+++ Prefabs/Objects/Clickable.gd
@@ -3,9 +3,15 @@ class_name Clickable
 
 signal clicked
 
+var is_clickable: bool = true setget set_is_clickable
+
+func set_is_clickable(value: bool) -> void:
+	is_clickable = value
+
 
 func _on_Clickable_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
 	and event.pressed:
-		emit_signal("clicked")
+		if is_clickable:
+			emit_signal("clicked")
