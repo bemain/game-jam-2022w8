c146ef504a00be8ba44c6b37e2de9416bf2ad7e4
BeMain <bemain.dev@gmail.com>
2022-03-19 16:46:06 +0100

    Update Locker1 to reflect changes to prefabs

--- Rooms/Room1/Locker1.gd
+++ Rooms/Room1/Locker1.gd
@@ -7,17 +7,17 @@ var is_open: bool = false
 
 
 func _ready() -> void:
-	$Lock/AnimatedSprite.animation = "Locked"
+	$LockFocus/Lock/AnimatedSprite.animation = "Locked"
 	$Sprite.visible = false
-	$TopShelf.visible = false
-	$BottomShelf.visible = false
+	$TopShelf.is_clickable = false  # Disable shelves
+	$BottomShelf.is_clickable = false
 
 
 func _on_Lock_unlocked() -> void:
-	$Lock/AnimatedSprite.animation = "Unlocked"
-	$TopShelf.visible = true  # Enable shelves
-	$BottomShelf.visible = true
-	$Lock/LockFocus.visible = false  # Disable lock focus
+	$LockFocus/Lock/AnimatedSprite.animation = "Unlocked"
+	$TopShelf.is_clickable = true  # Enable shelves
+	$BottomShelf.is_clickable = true
+	$LockFocus.is_clickable = false  # Disable LockFocus
 	
 	$SoundEffectPlayer.play_effect("LockSound")  # Play sound effect
 
@@ -27,7 +27,7 @@ func _on_shelf_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) 
 	and event.button_index == BUTTON_LEFT \
 	and event.pressed:
 		# If Lock is clicked while already unlocked, open the locker
-		if $Lock.is_locked or is_open:
+		if $LockFocus/Lock.is_locked or is_open:
 			return
 		yield(get_tree(),"idle_frame")  # Wait one frame before focusing to let input pass to children first
 		is_open = true
@@ -36,9 +36,9 @@ func _on_shelf_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) 
 
 func _on_opened() -> void:
 	$Sprite.visible = true
-	$Lock.position = Vector2(0, 380)  # Position Lock on the floor
-	$Lock.rotation_degrees = 90
-	$Lock/AnimatedSprite.animation = "On floor"
+	$LockFocus/Lock.position = Vector2(0, 380)  # Position Lock on the floor
+	$LockFocus/Lock.rotation_degrees = 90
+	$LockFocus/Lock/AnimatedSprite.animation = "On floor"
 	
 	$SoundEffectPlayer.play_effect("LockerOpenSound")  # Play sound effect
 
